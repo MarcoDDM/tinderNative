fcd3f4912f77961e6cf0d8a30c89ae1269b6ee40
Mridul11 <mridulmishra2991@gmail.com>
2018-03-26 13:36:14 +0530

    commiting after completing full swipe function

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -6,10 +6,16 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight : () => {},
+    onSwipeLeft : () => {}
+  }
+
   constructor(props){
     super(props);
 
     const position = new Animated.ValueXY();
+
     const panResponder = PanResponder.create({
       //involve with user interaction with elem
       onStartShouldSetPanResponder : () => true,
@@ -32,7 +38,7 @@ class Deck extends Component {
         }
     });
 
-    this.state = { panResponder, position };
+    this.state = { panResponder, position, index:0 };
   }
 
   forceSwipe(direction){
@@ -45,9 +51,12 @@ class Deck extends Component {
   }
 
   onSwipeComplete(direction){
-    const {onSwipeLeft, onSwipeRight} = this.props;
+    const {onSwipeLeft, onSwipeRight, data} = this.props;
+    const item = data[this.state.index];
 
-   direction === 'right' ? onSwipeRight() : onSwipeLeft();
+   direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+   this.state.position.setValue({x :0, y : 0});
+   this.setState({index : this.state.index + 1});
   }
 
   resetPosition(){
@@ -57,7 +66,7 @@ class Deck extends Component {
   }
 
   getCardStyle() {
-    const {position} = this.state;
+    const { position } = this.state;
     const rotate = position.x.interpolate({
       inputRange: [-SCREEN_WIDTH * 1.5 , 0 , SCREEN_WIDTH * 1.5],
       outputRange: ['-120deg', '0deg','120deg']
@@ -70,8 +79,11 @@ class Deck extends Component {
 }
 
   renderCards(){
-    return this.props.data.map((item , index) => {
-      if(index === 0) {
+    return this.props.data.map((item , i) => {
+      if(i < this.state.index){
+        return null
+      }
+      if(i === this.state.index) {
         return(
             <Animated.View
                   key = {item.id}
